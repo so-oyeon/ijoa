5e41c0f6e35bf2203f5d57cdf4a0c46db35bab11
이진주 <dlwlswn3539@naver.com>
2024-10-25 13:54:36 +0900

    [BE] Feat: 기본 응답 dto 구현

---
+++ backend/src/main/java/com/checkitout/ijoa/dto/ResponseDto.java
@@ -0,0 +1,25 @@
+package com.checkitout.ijoa.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class ResponseDto {
+
+    @Schema(description = "응답 상태", example = "SUCCESS")
+    private String status;
+
+    @Schema(description = "응답 메시지", example = "요청 처리 완료.")
+    private String message;
+
+    public ResponseDto() {
+        this.status = "SUCCESS";
+        this.message = "요청 처리 완료";
+    }
+}
