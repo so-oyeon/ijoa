e733b10a0b1160c3bb5b3bc5da46a34dae46af59
이진주 <dlwlswn3539@naver.com>
2024-10-25 16:32:01 +0900

    [BE] Feat: 회원 엔티티 구현

---
+++ backend/src/main/java/com/checkitout/ijoa/domain/User.java
@@ -0,0 +1,45 @@
+package com.checkitout.ijoa.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private boolean isDeactivated;
+
+    public static User createUser(String email, String password, String nickname, LocalDateTime now) {
+        User user = new User();
+        user.email = email;
+        user.password = password;
+        user.nickname = nickname;
+        user.createdAt = now;
+        user.updatedAt = now;
+        user.isDeactivated = false;
+        return user;
+    }
+}
