460878bb3951159d10b6f057fb403a9a808d4ad9
이진주 <dlwlswn3539@naver.com>
2024-10-26 14:33:07 +0900

    [BE] Feat: 인증코드 전송 구현

--- backend/src/main/java/com/checkitout/ijoa/exception/ErrorCode.java
+++ backend/src/main/java/com/checkitout/ijoa/exception/ErrorCode.java
@@ -9,8 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     //user
-    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER-001", "이미 사용 중인 이메일입니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-002", "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-001", "사용자를 찾을 수 없습니다."),
+
+    //email
+    EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "EMAIL-001", "이미 사용 중인 이메일입니다."),
+    EMAIL_VERIFICATION_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "EMAIL-002", "인증번호 발송에 실패했습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
