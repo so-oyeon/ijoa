4cf872451ee678803ff571dfa37cf182f844ca37
김민솔 <artkms4815@naver.com>
2024-10-28 01:28:49 +0900

    Refactor: 도커파일 코드수정

--- TTS/TTS_script.py
+++ TTS/TTS_script.py
@@ -1,10 +1,13 @@
+import os
 from TTS.api import TTS
-tts = TTS("/app/xtts_model",gpu=False)
+
+
+tts = TTS("/app/xtts_models",gpu=False)
 
 # generate speech by cloning a voice using default settings
 tts.tts_to_file(text="우주 고양이 아주 잘 만든 거 볼 수 있죠?",
-                file_path="/workspace/output/output.wav",
-                speaker_wav=["/workspace/dataset/wavs/audio1.wav"],
+                file_path=os.path.join(os.getenv("OUTPUT_PATH"), "output.wav"),
+                speaker_wav=["/app/dataset/wavs/audio1.wav"],
                 language="ko",
                 split_sentences=True
                 )
--- TTS/train.py
+++
@@ -1,7 +0,0 @@
-from TTS.api import Trainer
-
-# config.json 파일 경로와 모델 경로 지정
-trainer = Trainer(config_path="/workspace/config.json", model_path="/app/tts_models/ko/kss/xtts")
-
-# 훈련 시작
-trainer.train()
