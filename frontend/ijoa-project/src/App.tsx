92fffe8f7850b2c608b96a0e7ff9224e5c2f694f
elumiryy <yerim5988jg@naver.com>
2024-10-28 17:51:26 +0900

    [FE] Design: 부모 Header 및 부모의 자녀 목록 화면 라우팅

--- frontend/ijoa-project/src/App.tsx
+++ frontend/ijoa-project/src/App.tsx
@@ -1,10 +1,30 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import ParentHeader from "./components/common/ParentHeader";
+import CreateChildProfile from "./pages/parent/ChildProfileList";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* 페이지에 Header가 포함된 화면 */}
+        <Route
+          path="/*"
+          element={
+            <>
+              <ParentHeader />
+              <div>
+                <Routes>
+                  <Route>
+                    {/* 부모의 자녀 목록 화면 */}
+                    <Route path="/parent/child/list" element={<CreateChildProfile />} />
+                  </Route>
+                </Routes>
+              </div>
+            </>
+          }
+        />
+
         {/* 홈 화면 */}
         <Route path="/" element={<Home />} />
       </Routes>
