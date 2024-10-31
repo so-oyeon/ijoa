f7c77e9d408f8022d93b735a407e118a81ace971
elumiryy <yerim5988jg@naver.com>
2024-10-31 16:22:27 +0900

    [FE] Design: 음성앨범 자녀 답변 디자인

--- frontend/ijoa-project/src/components/parent/voiceAlbum/VoiceAlbumDetailCard.tsx
+++ frontend/ijoa-project/src/components/parent/voiceAlbum/VoiceAlbumDetailCard.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { VoiceAlbumDetailCardInfo } from "../../../types/voiceAlbum";
+import { BiSolidRightArrow } from "react-icons/bi";
 
 interface Props {
   voiceInfo: VoiceAlbumDetailCardInfo;
@@ -18,10 +19,25 @@ const VoiceAlbumDetailCard = ({ voiceInfo }: Props) => {
 
   return (
     <div className="h-full p-5 bg-white rounded-2xl grid grid-rows-[2fr_1fr_1fr]">
+      {/* 동화책 그림 이미지 */}
       <img className="w-full aspect-2 rounded-xl object-cover" src={`/assets/${voiceInfo.img}.png`} alt="" />
+
+      {/* 퀴즈 질문 */}
       <div className="grid grid-cols-[1fr_4fr] gap-5 place-items-center">
         <img className="w-14" src={`/assets/fairytales/images/${animalImage}.png`} alt="" />
         <p className="text-lg text-left break-keep">{voiceInfo.question}</p>
+      </div>
+
+      {/* 퀴즈 답변 */}
+      <div className="flex justify-end items-center space-x-3">
+        {/* 재생 버튼 */}
+        <div className="w-10 aspect-1 bg-[#FFCA75] flex justify-center items-center rounded-full">
+          <BiSolidRightArrow className="text-2xl text-white" />
+        </div>
+        {/* 음성 그래프 */}
+        <img src="/assets/parent/voiceAlbum/voice-graph.png" alt="" />
+        {/* 자녀 프로필 이미지 */}
+        <img className="w-14 h-14 border rounded-full" src="/assets/profile-img-girl.png" alt="" />
       </div>
     </div>
   );
