f18199d332724888cb98fb7588397211587d1ce4
권윤하 <ellen4421@naver.com>
2024-10-31 14:43:11 +0900

    [FE] Design: 회원가입, 로그인 등 css 전반 수정
    - 스플래쉬 화면 css 수정
    - 회원가입 모달 css 수정
    - 로그인 모달 css 수정
    - 비밀번호 찾기 모달 css 수정
    - 부모 화면 설정 모달 css 수정
    - 본인 확인 모달 css 수정
    - 회원 정보 변경 모달 css 수정
    - 회원 탈퇴 모달 css 수정

--- frontend/ijoa-project/src/components/user/NotFoundModal.tsx
+++ frontend/ijoa-project/src/components/user/NotFoundModal.tsx
@@ -1,16 +1,17 @@
 import React from "react";
+import Attention from "/assets/fairytales/buttons/attention.png";
 
 interface NotFoundModalProps {
   onClose: () => void;
 }
 
-const NotFoundModal: React.FC<NotFoundModalProps> = ({ onClose }) => {
+const NotFoundModal: React.FC<NotFoundModalProps> = () => {
   return (
     <>
-      <p className="font-bold text-2xl mb-4 text-red-600">존재하지 않는 계정입니다.</p>
-      <button onClick={onClose} className="w-3/5 py-3 font-bold bg-yellow-400 rounded-full hover:bg-yellow-500">
-        닫기
-      </button>
+      <div className="flex justify-center items-center">
+        <img src={Attention} alt="주의 아이콘" className="w-24 mb-4" />
+      </div>
+        <p className="font-bold text-2xl mb-4">존재하지 않는 계정입니다.</p>
     </>
   );
 };
