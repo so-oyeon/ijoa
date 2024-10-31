c5f1b7e0dd4ba5fa448656999a7d37477a5ac8ba
김소연 <xhwkrlxhdi@naver.com>
2024-10-31 23:13:24 +0900

    [BE] Refactor: DTO 변경

--- backend/src/main/java/com/checkitout/ijoa/fairytale/dto/PageHistoryCreationRequest.java
+++ backend/src/main/java/com/checkitout/ijoa/fairytale/dto/PageHistoryCreationRequest.java
@@ -2,37 +2,39 @@ package com.checkitout.ijoa.fairytale.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
+@NoArgsConstructor
 @Schema(description = "시선추척 데이터 요청")
 public class PageHistoryCreationRequest {
 
     @NotNull(message = "얼굴 미인식 여부는 필수 항목입니다.")
     @Schema(description = "얼굴 미인식 여부", example = "false")
-    private final Boolean isFaceMissing;
+    private Boolean isFaceMissing;
 
     @Schema(description = "화면 밖 응시 여부", example = "false")
-    private final Boolean isGazeOutOfScreen;
+    private Boolean isGazeOutOfScreen;
 
     @Schema(description = "x 좌표", example = "1.1")
-    private final Float gazeX;
+    private Float gazeX;
 
     @Schema(description = "y 좌표", example = "1.1")
-    private final Float gazeY;
+    private Float gazeY;
 
     @Schema(description = "동공 크기", example = "3.1")
-    private final Float pupilSize;
+    private Float pupilSize;
 
     @Schema(description = "집중도", example = "1.0")
-    private final Float attentionRate;
+    private Float attentionRate;
 
     @Schema(description = "단어", example = "사과")
-    private final String word;
+    private String word;
 
     @NotNull(message = "그림 여부는 필수 항목입니다.")
     @Schema(description = "그림 여부", example = "false")
-    private final Boolean isImage;
+    private Boolean isImage;
 }
