b7a2c347d49322256a7b216f3ab61f78196ce2e8
elumiryy <yerim5988jg@naver.com>
2024-11-01 09:09:59 +0900

    [FE] Design: 독서 통계의 히스토그램 디자인

---
+++ frontend/ijoa-project/src/pages/parent/ReadingStats.tsx
@@ -0,0 +1,11 @@
+import Histogram from "../../components/parent/stats/Histogram";
+
+const ReadingStats = () => {
+  return (
+    <div className="px-20 pt-24 pb-10 h-screen">
+      <Histogram />
+    </div>
+  );
+};
+
+export default ReadingStats;
