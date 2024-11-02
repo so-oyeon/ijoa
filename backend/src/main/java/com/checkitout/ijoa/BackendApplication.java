5681bc9ae2e08b6d760866f8c84f3dd3caca7e27
이진주 <dlwlswn3539@naver.com>
2024-11-02 11:29:54 +0900

    [BE] Chore: Swagger https주소 설정

--- backend/src/main/java/com/checkitout/ijoa/BackendApplication.java
+++ backend/src/main/java/com/checkitout/ijoa/BackendApplication.java
@@ -3,6 +3,9 @@ package com.checkitout.ijoa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://k11d105.p.ssafy.io/api/v1", description = "default server url"),
+        @Server(url = "http://localhost:8080/api/v1", description = "LOCAL")
+})
 @SpringBootApplication
 public class BackendApplication {
 
