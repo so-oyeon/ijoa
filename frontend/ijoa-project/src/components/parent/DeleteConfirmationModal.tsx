7c3918cca1a5e9e4c21d7ec3620005cfe3a78503
김민수 <alstn0575@naver.com>
2024-11-04 15:13:00 +0900

    [FE] Feat: 회원탈퇴 통신 연결 완료
    - 회원가입 통신 연결 완료
    - 누락된 페이지 추가 및 수정 완료

---
+++ frontend/ijoa-project/src/components/parent/DeleteConfirmationModal.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { userApi } from "../../api/userApi";
+
+interface DeleteConfirmationModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({ isOpen, onClose }) => {
+  const navigate = useNavigate();
+
+  const confirmDeleteAccount = async () => {
+    try {
+      const response = await userApi.deleteUserInfo();
+      if (response.status === 200) {
+        localStorage.clear();
+        Swal.fire({
+          icon: "success",
+          title: "회원 탈퇴가 완료되었습니다",
+          confirmButtonText: "확인",
+        }).then(() => {
+          navigate("/home");
+        });
+      } else {
+        throw new Error("회원 탈퇴 실패");
+      }
+    } catch (error) {
+      console.error("회원 탈퇴 실패:", error);
+      Swal.fire({
+        icon: "error",
+        title: "회원 탈퇴 실패",
+        text: "다시 시도해 주세요.",
+        confirmButtonText: "확인",
+      });
+    }
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="relative w-1/3 py-8 bg-white rounded-lg text-center p-6">
+        <h1 className="text-xl font-bold text-gray-700 mb-4">회원 탈퇴</h1>
+        <p className="text-gray-600 font-semibold mb-6">
+          정말로 회원 탈퇴를 진행하시겠습니까? 이 작업은 되돌릴 수 없습니다.
+        </p>
+        <button
+          className="w-1/3 h-[60px] py-2 bg-red-600 text-white rounded-full font-bold hover:bg-red-700 mb-4"
+          onClick={confirmDeleteAccount}
+        >
+          탈퇴
+        </button>
+        <button
+          className="w-1/3 h-[60px] py-2 bg-gray-300 text-gray-700 rounded-full font-bold hover:bg-gray-400"
+          onClick={onClose}
+        >
+          취소
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteConfirmationModal;
