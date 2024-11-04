123509d9b9f5b2ad0c3d35ea202f0d4903ab92cd
elumiryy <yerim5988jg@naver.com>
2024-11-04 13:36:07 +0900

    [FE] Feat: 자녀 프로필 생성 API 함수 추가

--- frontend/ijoa-project/src/api/childApi.ts
+++ frontend/ijoa-project/src/api/childApi.ts
@@ -1,7 +1,13 @@
 import api from "../lib/axios";
 
 export const childApi = {
+  // 자녀 프로필 목록 조회
   getChildList: () => {
     return api.get(`/parent/children`);
+  },
+
+  // 자녀 프로필 생성
+  createChildProfile: (formData: FormData) => {
+    return api.post(`/parent/children`, formData)
   },
 }
