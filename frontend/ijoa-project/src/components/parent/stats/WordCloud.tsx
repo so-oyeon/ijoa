cdb51a26e1e4c56c4505f447c9320ad9aa11728f
elumiryy <yerim5988jg@naver.com>
2024-11-05 17:25:02 +0900

    [FE] Design: 통계 페이지의 워드 클라우드 로딩 애니메이션 추가

--- frontend/ijoa-project/src/components/parent/stats/WordCloud.tsx
+++ frontend/ijoa-project/src/components/parent/stats/WordCloud.tsx
@@ -1,3 +1,6 @@
+import Lottie from "react-lottie-player";
+import loadingAnimation from "../../../lottie/footPrint-loadingAnimation.json";
+
 const WordCloud = () => {
   return (
     <div className="pt-5 flex flex-col relative">
@@ -5,6 +8,10 @@ const WordCloud = () => {
         <p className="w-2/3 py-2 text-center font-semibold bg-[#B1EBAB] rounded-full absolute top-0 left-1/2 transform -translate-x-1/2">
           이런 책이 재밌어요
         </p>
+
+        <div className="w-full h-full flex justify-center items-center">
+          <Lottie className="w-40 aspect-1" loop play animationData={loadingAnimation} />
+        </div>
       </div>
     </div>
   );
