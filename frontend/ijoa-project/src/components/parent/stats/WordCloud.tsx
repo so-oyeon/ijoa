a234f0174443b073f9a9c673987906b15f1f5886
elumiryy <yerim5988jg@naver.com>
2024-11-05 17:23:28 +0900

    [FE] Refactor: 통계 페이지의 워드 클라우드 컴포넌트화

---
+++ frontend/ijoa-project/src/components/parent/stats/WordCloud.tsx
@@ -0,0 +1,13 @@
+const WordCloud = () => {
+  return (
+    <div className="pt-5 flex flex-col relative">
+      <div className="grow p-3 border-4 border-[#F5F5F5] rounded-2xl">
+        <p className="w-2/3 py-2 text-center font-semibold bg-[#B1EBAB] rounded-full absolute top-0 left-1/2 transform -translate-x-1/2">
+          이런 책이 재밌어요
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WordCloud;
