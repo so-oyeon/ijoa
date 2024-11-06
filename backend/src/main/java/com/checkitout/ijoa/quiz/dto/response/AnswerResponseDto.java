a279cf2564fad1e0a7b877d5b30e308ac325596b
김민솔 <artkms4815@naver.com>
2024-11-06 10:40:17 +0900

    feat : 동화책 답변목록 조회

--- backend/src/main/java/com/checkitout/ijoa/quiz/dto/response/AnswerResponseDto.java
+++ backend/src/main/java/com/checkitout/ijoa/quiz/dto/response/AnswerResponseDto.java
@@ -1,24 +1,40 @@
 package com.checkitout.ijoa.quiz.dto.response;
 
+import com.checkitout.ijoa.fairytale.domain.Fairytale;
+import com.checkitout.ijoa.quiz.domain.Answer;
+import com.checkitout.ijoa.quiz.domain.Quiz;
 import lombok.Builder;
 import lombok.Data;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
 
 @Data
 public class AnswerResponseDto {
     private Long answerId;
     private Long fairytaleId;
     private Long quizId;
-    private String text;
+    private String questionText;
     private String image;
     private String answer;
 
     @Builder
-    public AnswerResponseDto(Long answerId, Long fairytaleId, Long quizId, String text, String image, String answer) {
+    public AnswerResponseDto(Long answerId, Long fairytaleId, Long quizId, String questionText, String image, String answer) {
         this.answerId = answerId;
         this.fairytaleId = fairytaleId;
         this.quizId = quizId;
-        this.text = text;
+        this.questionText = questionText;
         this.image = image;
         this.answer = answer;
     }
+
+    public static AnswerResponseDto from(Answer answer, Fairytale fairytale,String answerurl) {
+        return AnswerResponseDto.builder()
+                .answerId(answer.getId())
+                .fairytaleId(fairytale.getId())
+                .quizId(answer.getQuiz().getId())
+                .questionText(answer.getQuiz().getQuestion())
+                .image(answer.getQuiz().getPage().getFairytalePageImage().getImageUrl())
+                .answer(answerurl)
+                .build();
+    }
+
 }
