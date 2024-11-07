8e393fce8a89b43a60c7f3abfb48056954ae77f2
elumiryy <yerim5988jg@naver.com>
2024-11-07 16:15:51 +0900

    [FE] Feat: 음성 앨범 답변한 책 목록 조회 통신 기능 추가

--- frontend/ijoa-project/src/types/voiceAlbumTypes.ts
+++ frontend/ijoa-project/src/types/voiceAlbumTypes.ts
@@ -3,3 +3,16 @@ export interface VoiceAlbumDetailCardInfo {
   question: string;
   answer: string;
 }
+
+// 음성 앨범 답변한 책 목록 조회 요청 데이터
+export interface VoiceAlbumDateRange {
+  startDate: string;
+  endDate: string;
+}
+
+// 음성 앨범 답변한 책 목록 조회 응답 데이터
+export interface VoiceAlbumBookInfo {
+  book_id: number;
+  image: string;
+  title: string;
+}
