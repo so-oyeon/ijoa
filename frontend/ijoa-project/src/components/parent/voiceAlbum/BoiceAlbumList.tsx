3077af9c67035434b129f471bfc4039c9763de95
elumiryy <yerim5988jg@naver.com>
2024-11-07 16:18:31 +0900

    [FE] Feat: 음성 앨범 답변한 책 목록 조회 통신 기능 구현

--- frontend/ijoa-project/src/components/parent/voiceAlbum/BoiceAlbumList.tsx
+++ frontend/ijoa-project/src/components/parent/voiceAlbum/BoiceAlbumList.tsx
@@ -1,12 +1,13 @@
 import { PiVinylRecord } from "react-icons/pi";
 import { useNavigate } from "react-router-dom";
+import { VoiceAlbumBookInfo } from "../../../types/voiceAlbumTypes";
 
 interface Props {
   topSize: number;
-  startIdx: number;
+  partBookList: VoiceAlbumBookInfo[];
 }
 
-const BoiceAlbumList = ({ topSize, startIdx }: Props) => {
+const BoiceAlbumList = ({ topSize, partBookList }: Props) => {
   const navigate = useNavigate();
   const handleGoToVoiceAlbumDetail = (id: number) => {
     navigate(`/parent/voice/album/${id}`);
@@ -18,19 +19,15 @@ const BoiceAlbumList = ({ topSize, startIdx }: Props) => {
       <div
         className="w-full px-5 grid grid-cols-4 place-items-center absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
         style={{ top: `${topSize}%` }}>
-        {Array.from({ length: 4 }, (_, i) => i + startIdx).map((num, index) => (
+        {partBookList.map((book, index) => (
           <div className="w-40 h-48 p-3 relative" key={index} onClick={() => handleGoToVoiceAlbumDetail(index)}>
             <div className="w-full h-full rounded-2xl shadow-[0_5px_3px_1px_rgba(0,0,0,0.2)]">
               {/* 동화책 표지 */}
-              <img
-                className="w-full h-3/5 rounded-t-2xl object-cover"
-                src={`/assets/fairytales/images/dummy${num + 1}.png`}
-                alt=""
-              />
+              <img className="w-full h-3/5 rounded-t-2xl object-cover" src={book.image} alt="" />
 
               {/* 동화책 제목 */}
               <div className="w-full h-2/5 bg-white rounded-b-2xl flex justify-center items-center">
-                <p className="w-full px-3 text-[#B27F44] text-center break-keep line-clamp-2">백설공주와 일곱 난쟁이</p>
+                <p className="w-full px-3 text-[#B27F44] text-center break-keep line-clamp-2">{book.title}</p>
               </div>
             </div>
 
