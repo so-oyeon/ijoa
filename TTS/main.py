ce0b7bd49ae2097f55fc1a495a93e3e94959a77e
김민솔 <artkms4815@naver.com>
2024-11-11 00:38:39 +0900

    Feat : kafka 연결

--- TTS/main.py
+++ TTS/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from confluent_kafka import Consumer, Producer
 import json
 import threading
-import tts_processor
+import tts_processor as tts_processor
+import create_tts as create_tts
 
 app = FastAPI()
 
 # Kafka 설정
 consumer_conf = {
-    'bootstrap.servers': 'k11d105.p.ssafy.io:9092',
+    'bootstrap.servers': '54.180.201.1:9092',
     'group.id': 'tts_group',
     'auto.offset.reset': 'earliest'
 }
@@ -33,20 +34,8 @@ def consume_messages():
         if topic == 'tts_create_audio':
             tts_processor.process_tts(request_data)
         elif topic == 'create_tts':
-            # request_data = {
-            #     "tts_id": 1,
-            # 	"path": [
-            #     		"train/1/9f4b4256-c041-41d2-bd8a-6276b42d5f25/audio1.wav",
-            #     		"train/1/9400403c-7f0f-4248-830d-babea8fbd524/audio2.wav",
-            #         	"train/1/3b61df6a-79c5-422e-aca2-973d8d2c2066/audio3.wav"
-            #         	],
-            # 	"script": [
-            #     		"우주 고양이 아주 잘 만든 거 볼 수 있죠?",
-            #     		"제가 얼마 전에 써봤을 때보다 성능이 굉장히 좋아진 것 같은데요?",
-            #         	"이렇게 되게 말끔하게 나왔습니다"
-            # 	]
-            # }
             create_tts.train_tts_model(request_data)
+
 
 # FastAPI 서버와 별도로 Kafka 컨슈머 스레드 실행
 threading.Thread(target=consume_messages, daemon=True).start()
