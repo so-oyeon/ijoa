fe74362c846cff972f9e752ce887623086776f04
김민수 <alstn0575@naver.com>
2024-11-11 17:10:11 +0900

    [FE] Feat: 스플래쉬 페이지 반응형 디자인 개발
    - 스플래쉬 페이지 반응형 디자인 개발

--- frontend/ijoa-project/src/pages/users/SplashScreen.tsx
+++ frontend/ijoa-project/src/pages/users/SplashScreen.tsx
@@ -20,25 +20,32 @@ const SplashScreen: React.FC = () => {
     <div
       className={`splash-screen relative w-screen h-screen bg-[#f3fbff] overflow-hidden ${fadeOut ? "fade-out" : ""}`}
     >
-      <div className="absolute top-[30%] right-[15%] w-[30vw] flex flex-col items-center">
-        <div className="flex space-x-2 text-[140px] font-['SDGothic'] text-white-stroke">
-          <span className="text-[#FFCC00] ">아</span>
-          <span className="text-[#99CC66] ">이</span>
-          <span className="text-[#FF6666] ">조</span>
-          <span className="text-[#339999] ">아</span>
+      {/* 로고 텍스트 */}
+      <div className="absolute top-[25%] right-[10%] w-[60vw] md:w-[40vw] lg:w-[30vw] flex flex-col items-center">
+        <div className="flex space-x-2 text-[15vw] md:text-[10vw] lg:text-[8vw] font-['SDGothic'] text-white-stroke">
+          <span className="text-[#FFCC00]">아</span>
+          <span className="text-[#99CC66]">이</span>
+          <span className="text-[#FF6666]">조</span>
+          <span className="text-[#339999]">아</span>
         </div>
-        <p className="w-full font-['MapleBold'] text-center font-semibold text-[#565656] text-2xl tracking-widest">
+        <p className="w-full font-['MapleBold'] text-center font-semibold text-[#565656] text-[4vw] md:text-2xl lg:text-3xl tracking-widest">
           엄마, 아빠가 읽어주는 동화책
         </p>
       </div>
 
-      <div className="flex items-end absolute left-[10%] bottom-[15%] gap-[0vw] z-10">
-        <img src={Tiger} alt="호랑이" className="character w-[12vw] mb-[17vw] -ml-[3vw]" />
-        <img src={Elephant} alt="코끼리" className="character w-[12vw] mb-[20vw] -ml-[3vw]" />
-        <img src={Giraffe} alt="기린" className="character w-[12vw] mb-[23vw] -ml-[3vw]" />
+      {/* 동물 이미지 */}
+      <div className="flex items-end absolute left-[5%] bottom-[15%] gap-[2vw] md:gap-[1vw] z-10">
+        <img src={Tiger} alt="호랑이" className="character w-[15vw] md:w-[10vw] lg:w-[8vw] mb-[13vw] -ml-[3vw]" />
+        <img src={Elephant} alt="코끼리" className="character w-[15vw] md:w-[10vw] lg:w-[8vw] mb-[16vw] -ml-[3vw]" />
+        <img src={Giraffe} alt="기린" className="character w-[15vw] md:w-[10vw] lg:w-[8vw] mb-[19vw] -ml-[3vw]" />
       </div>
 
-      <img src={Tablet} alt="태블릿 화면" className="absolute bottom-[5%] w-[45vw] translate-y-[10%]" />
+      {/* 태블릿 이미지 */}
+      <img
+        src={Tablet}
+        alt="태블릿 화면"
+        className="absolute bottom-[5%] w-[50vw] md:w-[40vw] lg:w-[35vw] translate-y-[10%]"
+      />
     </div>
   );
 };
