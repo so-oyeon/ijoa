f173ef2145836465b2ae778c3de5eaa9c1efae34
elumiryy <yerim5988jg@naver.com>
2024-11-12 16:15:25 +0900

    [FE] Chore: vite.config.ts의 COOP 및 COEP 헤더 설정 제거

--- frontend/ijoa-project/vite.config.ts
+++ frontend/ijoa-project/vite.config.ts
@@ -13,10 +13,4 @@ export default defineConfig({
       },
     }),
   ],
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-    },
-  },
 });
