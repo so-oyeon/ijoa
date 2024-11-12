9360710e20302ab4ac446a37e9106c931eed6e5f
권윤하 <ellen4421@naver.com>
2024-11-12 15:21:08 +0900

    [FE] Feat: 스플래쉬 화면 사진 변경 및 반응형 구현
    - 스플래쉬 화면 사진 변경 및 반응형 구현

--- frontend/ijoa-project/src/pages/users/SplashScreen.tsx
+++ frontend/ijoa-project/src/pages/users/SplashScreen.tsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Tiger from "/assets/user/tiger.png";
-import Elephant from "/assets/user/elephant.png";
-import Giraffe from "/assets/user/giraffe.png";
-import Tablet from "/assets/user/tablet.png";
+import Tablet from "/assets/user/tablet2.png";
 import "../../css/Login.css";
 
 const SplashScreen: React.FC = () => {
@@ -20,25 +17,23 @@ const SplashScreen: React.FC = () => {
     <div
       className={`splash-screen relative w-screen h-screen bg-[#f3fbff] overflow-hidden ${fadeOut ? "fade-out" : ""}`}
     >
-      <div className="absolute top-[30%] right-[15%] w-[30vw] flex flex-col items-center">
-        <div className="flex space-x-2 text-[140px] font-['SDGothic'] text-white-stroke">
-          <span className="text-[#FFCC00] ">아</span>
-          <span className="text-[#99CC66] ">이</span>
-          <span className="text-[#FF6666] ">조</span>
-          <span className="text-[#339999] ">아</span>
+      <div className="absolute top-[10%] sm:top-[30%] right-[10%] sm:right-[15%] w-[70vw] sm:w-[50vw] md:w-[30vw] flex flex-col items-center">
+        <div className="flex space-x-1 sm:space-x-2 text-[60px] sm:text-[100px] lg:text-[140px] font-['SDGothic'] text-white-stroke">
+          <span className="text-[#FFCC00]">아</span>
+          <span className="text-[#99CC66]">이</span>
+          <span className="text-[#FF6666]">조</span>
+          <span className="text-[#339999]">아</span>
         </div>
-        <p className="w-full font-['MapleBold'] text-center font-semibold text-[#565656] text-2xl tracking-widest">
+        <p className="w-full font-['MapleBold'] text-center font-semibold text-[#565656] text-md sm:text-lg md:text-2xl tracking-widest mt-4 whitespace-nowrap">
           엄마, 아빠가 읽어주는 동화책
         </p>
       </div>
 
-      <div className="flex items-end absolute left-[10%] bottom-[15%] gap-[0vw] z-10">
-        <img src={Tiger} alt="호랑이" className="character w-[12vw] mb-[17vw] -ml-[3vw]" />
-        <img src={Elephant} alt="코끼리" className="character w-[12vw] mb-[20vw] -ml-[3vw]" />
-        <img src={Giraffe} alt="기린" className="character w-[12vw] mb-[23vw] -ml-[3vw]" />
-      </div>
-
-      <img src={Tablet} alt="태블릿 화면" className="absolute bottom-[5%] w-[45vw] translate-y-[10%]" />
+      <img
+        src={Tablet}
+        alt="태블릿 화면"
+        className="absolute -bottom-[10%] sm:-bottom-[20%] -left-[10%] sm:-left-[5%] w-[80vw] sm:w-[60vw] md:w-[40vw] translate-y-[10%]"
+      />
     </div>
   );
 };
