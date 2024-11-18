ca2e1a0639db2aac6f7b490691c4cb3cae6c40bf
김민솔 <artkms4815@naver.com>
2024-11-18 15:16:55 +0900

    Fix : quiz 목록 날짜 추가

--- backend/src/main/java/com/checkitout/ijoa/quiz/dto/request/QuizBookRequestDto.java
+++ backend/src/main/java/com/checkitout/ijoa/quiz/dto/request/QuizBookRequestDto.java
@@ -3,12 +3,14 @@ package com.checkitout.ijoa.quiz.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class QuizBookRequestDto {
 
-    @Schema(description = "시작 날짜", example = "2024-10-01")
+    @Schema(description = "시작 날짜", example = "2024-11-01T00:00:00")
     private String startDate;
 
-    @Schema(description = "종료 날짜", example = "2024-11-06")
-    private String endDate;
+    @Schema(description = "종료 날짜", example = "2024-11-30T23:59:59")
+    private String  endDate;
 }
